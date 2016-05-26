1ed8e6d4331d4e12f70b82d89e9f9c66b8890783
Bill Erickson <bill.erickson@gmail.com>
2016-05-26 12:09:29 -0500

    add plugin header

---
+++ wp-cli-install-missing.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: wp cli install missing
+ * Plugin URI:  https://github.com/billerickson/wp-cli-install-missing
+ * Description: Using wp cli, install any plugins that are "active" but missing.
+ * Version:     0.1.0
+ * Author:      Bill Erickson
+ * Author URI:  http://www.billerickson.net
+ * License: GPL v2.0
+ */
