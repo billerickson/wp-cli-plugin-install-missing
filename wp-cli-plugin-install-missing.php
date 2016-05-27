f0e1dc4d24d25d2afd7879b1325089589f653155
Daniel Bachhuber <daniel@handbuilt.co>
2016-05-27 14:12:25 -0700

    Trim trailing whitespace

--- wp-cli-plugin-install-missing.php
+++ wp-cli-plugin-install-missing.php
@@ -9,9 +9,9 @@
  * License: GPL v2.0
  */
 
-if ( ! ( defined('WP_CLI') && WP_CLI ) )
+if ( ! ( defined('WP_CLI') && WP_CLI ) ) {
 	return;
-	
+}
 
 /**
  * Installs any plugins that are active but missing
@@ -27,36 +27,34 @@ function be_wpcli_install_missing( $args, $assoc_args ) {
 	$response = WP_CLI::launch_self( 'plugin list', array(), array( 'format' => 'json', 'status' => 'active' ), false, true );
 	$active = json_decode( $response->stdout );
 	$active = wp_list_pluck( $active, 'name' );
-	
+
 	// Installed Plugins
 	$installed = get_option( 'active_plugins' );
 	foreach( $installed as &$plugin ) {
 		$plugin = strstr( $plugin, '/', true );
 	}
-		
+
 	// Missing Plugins
 	$missing = array_diff( $installed, $active );
 
-	// No Missing Plugins			
+	// No Missing Plugins
 	if( empty( $missing ) ) {
-
 		WP_CLI::success( 'No missing plugins' );
 		return;
-		
 	}
-	
-	// Display list of Missing Plugins	
+
+	// Display list of Missing Plugins
 	WP_CLI::log( 'The following plugins are missing:' );
 	foreach( $missing as $plugin ) {
 		WP_CLI::log( $plugin );
 	}
-	
+
 	// Quit here for dry run
 	$dry_run = isset( $assoc_args['dry-run'] ) && (bool) $assoc_args['dry-run'] ? true : false;
-	if( $dry_run ) {
+	if ( $dry_run ) {
 		return;
 	}
-		
+
 	// Install plugins
 	WP_CLI::log( 'Installing plugins...' );
 	foreach( $missing as $plugin ) {
